260eeadc873a87f6febed79813e242e645ef39bf
twarbelow <taija.warbelow@google.com>
2020-09-01 08:47:20 -0700

    index action for search controller

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,5 +1,5 @@
 class SearchController < ActionController::Base
   def index
-    
+    @results = HarryPotterService.new.search(params[:query]).map { |member| Member.new(member) } if params[:query].present?
   end
 end
